00ee09de9dc91ff09d61a50475f98c7e830ec69e
Alex <alexga1410@gmail.com>
2023-12-02 16:23:20 +0200

    Домашная работа к 3 семинару

---
+++ Home-work(sem-3)/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+
+double[] numbers = {3.14, 2.71, 5.55, 7.77, 1.25};
+
+
+double max = numbers[0]; 
+double min = numbers[0]; 
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+double difference = max - min;
+
+ Console.WriteLine($"Разница между максимальным и минимальным элементами: {difference}");
+    
---
+++ Home-work(sem-3)/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 4**(не обязательно): Дано натуральное число в диапазоне от 1 до 100 000. 
+Создайте массив, состоящий из цифр этого числа. 
+Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем. 
+Размер массива должен быть равен количеству цифр.*/
+
+Console.WriteLine("Введите натуральное число от 1 до 100000:");
+int number = int.Parse(Console.ReadLine());
+
+// Определяем количество цифр в числе
+int count = 0;
+int temp = number;
+while (temp > 0)
+{
+    temp /= 10;
+    count++;
+}
+
+int[] digits = new int[count];
+temp = number;
+
+for (int i = count - 1; i >= 0; i--)
+{
+    digits[i] = temp % 10;
+    temp /= 10;
+}
+
+Console.WriteLine("Массив цифр числа:");
+foreach (int digit in digits)
+
+    Console.Write(digit + " ");
