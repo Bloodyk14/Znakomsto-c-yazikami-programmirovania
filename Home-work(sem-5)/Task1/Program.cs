7d6473beb9a098886f3a0f58abc5b1a0fef2e49d
Alex <alexga1410@gmail.com>
2023-12-06 15:45:32 +0200

    Решил домашку 5сем

--- Home-work(sem-5)/Task1/Program.cs
+++ Home-work(sem-5)/Task1/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.*/
+using System;
+
+class Program
+{
+    static int[] GenerateRanArr(int size)
+    {
+        int[] array = new int[size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(1, 10); // Генерация трёхзначных чисел
+        }
+
+        return array;
+    }
+
+    static void DisplayArrAndCou(int[] array, int count)
+    {
+        Console.WriteLine("Массив случайных трёхзначных чисел:");
+        foreach (var number in array)
+        {
+            Console.Write(number + " ");
+        }
+
+        Console.WriteLine($"\nКоличество чётных чисел: {count}");
+    }
+
+ static int CountEvenNumbers(int[] array)
+    {
+        int count = 0;
+
+        foreach (var number in array)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
+    static void Main()
+    {
+       int size = 10; // Размер массива
+
+        int[] randomArray = GenerateRanArr(size);
+        int countOfEvenNumbers = CountEvenNumbers(randomArray);
+
+        DisplayArrAndCou(randomArray, countOfEvenNumbers);
+    }
+
+}
---
+++ Home-work(sem-5)/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 3: Напишите программу, которая перевернёт одномерный массив 
+(первый элемент станет последним, второй – предпоследним и т.д.)*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int [] mainArray = {0, 1, 2, 3, 4, 5};
+
+        Console.WriteLine("Исходный массив: ");
+        DisplayArray(mainArray);
+
+        int [] reversedArray = ReverseArray(mainArray);
+        
+        Console.WriteLine("Перевернутый массив: ");
+        DisplayArray(reversedArray);
+    }
+
+    static void DisplayArray(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static int[] ReverseArray(int[] arr)
+    {
+        int length = arr.Length;
+        int[] reversed = new int[length];
+
+        // Переворачиваем массив, меняя местами элементы
+        for (int i = 0; i < length; i++)
+        {
+            reversed[i] = arr[length - 1 - i];
+        }
+
+        return reversed;
+    }
+}
