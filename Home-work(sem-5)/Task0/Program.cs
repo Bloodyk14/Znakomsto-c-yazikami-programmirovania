e81b1b4d32bf33b8a9c2570c3d5c1ac79a8e65a6
Alex <alexga1410@gmail.com>
2023-12-08 01:01:31 +0200

    1/4 tasks

---
+++ Home-work(sem-5)/Task0/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.*/ 
+
+int[,] GetUserInputArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"Введите значение для элемента [{i},{j}]: ");
+            if (!int.TryParse(Console.ReadLine(), out array[i, j]))
+            {
+                Console.WriteLine("Некорректный ввод! Попробуйте еще раз.");
+                j--; // Повторяем запрос для текущего элемента
+            }
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string GetElementValue(int[,] array, int row, int column)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    // Проверяем, находится ли позиция в пределах массива
+    if (row >= 0 && row < rows && column >= 0 && column < columns)
+    {
+        return $"Значение элемента [{row},{column}]: {array[row, column]}";
+    }
+    else
+    {
+        return "Такого элемента нет в массиве.";
+    }
+}
+
+static int Workwithuser(string message)
+    {
+        Console.Write(message);
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+
+
+int[,] userArray = GetUserInputArray(3, 3);
+
+PrintArray(userArray); 
+Console.WriteLine();
+
+string elementInfo = GetElementValue(userArray, 3, 3); // Получение информации о значении элемента [1,1]
+Console.WriteLine(elementInfo); // Вывод результата работы GetElementValue
+
+
