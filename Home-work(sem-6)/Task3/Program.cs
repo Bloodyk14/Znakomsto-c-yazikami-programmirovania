e0bec27dc875225773e9f3eaf130631c2533b13f
Alex <alexga1410@gmail.com>
2023-12-12 19:14:39 +0200

    Finish home-work(sem6)

---
+++ Home-work(sem-6)/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 3: Задайте произвольную строку. Выясните,
+является ли она палиндромом.
+
+“aBcD1ef!-” => Нет
+“шалаш” => Да
+“55655” => Да*/
+
+static bool IsPalindrome(string str)
+{
+    int i = 0;
+    int j = str.Length - 1;
+
+    while (i < j)
+    {
+        if (str[i] != str[j])
+        {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+string str = "hahahahahah";
+bool isPalindrome = IsPalindrome(str.ToLower().Replace(" ", "").Replace(",", "").Replace(".", ""));
+
+if (isPalindrome)
+{
+    Console.WriteLine("=> Да");
+}
+else
+{
+    Console.WriteLine("=> Нет");
+}
